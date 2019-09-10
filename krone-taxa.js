868092953ae071c6cb2526f6ef4fd8d7da1e3a3b
laererlund <laererlund@gmail.com>
2019-09-10 08:06:57 +0200

    Rettelser i Taxameter.js samt uddybende kommentarer

--- krone-taxa.js
+++ krone-taxa.js
@@ -1,1 +1,6 @@
+/*
+ * Denne fil kalder en funktion i filen start.js, der ligger i library-mappen, 
+ * og som I ikke skal ændre ved i denne opgave. Til gengæld kunne man forestille 
+ * sig at der her blev indsat et
+ */
 start(new Taxameter());
